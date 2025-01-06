01c658bad498252f34914a48bfc4406bfd72faff
Junaid-Shaukat <junaiddshaukat@gmail.com>
2025-01-06 09:19:07 +0500

    refactor: update database connection handling and add build/start scripts

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -2,15 +2,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const dbUrl: string = "mongodb+srv://root:root@lms.j2ehk.mongodb.net/?retryWrites=true&w=majority&appName=LMS";
+
+const dbUrl: string = process.env.DB_URL || "";
 
 const mongodbconnection = async () => {
   try {
+    if (!dbUrl) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
+    }
+    
     const connection = await mongoose.connect(dbUrl);
     console.log(`Database connected successfully to host: ${connection.connection.host}`);
-  } catch (err:any) {
+  } catch (err: any) {
     console.log("Database connection error:", err.message);
-    
+    // Retry connection after 5 seconds
     setTimeout(mongodbconnection, 5000);
   }
 };
