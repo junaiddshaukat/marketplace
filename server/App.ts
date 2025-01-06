0370613a46e1bb71f14d700c37a99ffe79c875f2
Junaid-Shaukat <junaiddshaukat@gmail.com>
2025-01-06 16:37:22 +0500

    refactor: streamline middleware setup and improve error handling in App.ts

--- server/App.ts
+++ server/App.ts
@@ -1,29 +1,24 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
 import { errorHandlerMiddleware } from "./middleware/errore";
 import userrouter from "./routes/user-routr";
 import productAdRouter from "./routes/product-router";
 import paymentRouter from "./routes/payment-router";
-import './jobs/emailReminder'
+import './jobs/emailReminder';
 
 // Initialize Express app
 export const app = express();
 
-// Middleware for body parsing
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
-// Middleware for JSON requests
+// Middleware for parsing JSON and URL-encoded requests
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Middleware for cookie parsing
 app.use(cookieParser());
 
-
 // CORS configuration
-const allowedOrigins = ['http://localhost:3000','https://mama-marketplace.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://mama-marketplace.vercel.app'];
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -39,27 +34,32 @@ app.use(
 );
 
 // Test route
-app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+app.get("/test", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: "API working successfully",
   });
 });
-app.get("/",(req,res)=>{
-  res.send("server is running and up");
-})
+
+// Root route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running and up");
+});
 
 // User routes
 app.use("/user", userrouter);
 app.use("/payment", paymentRouter);
+
 // Product routes
 app.use("/product", productAdRouter);
 
 // Handle undefined routes
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
-  const err: any = new Error(`Route ${req.originalUrl} not found`);
-  err.statusCode = 404;
-  next(err);
+  const error = new Error(`Route ${req.originalUrl} not found`);
+  next({
+    statusCode: 404,
+    message: error.message,
+  });
 });
 
 // Error-handling middleware
